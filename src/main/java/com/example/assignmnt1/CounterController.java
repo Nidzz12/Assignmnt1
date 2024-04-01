5b62a952abe87ba089804cfbb87411e4741d9b23
Nidzz12 <157075668+Nidzz12@users.noreply.github.com>
2024-04-01 02:04:58 -0400

    Assignment01 done

---
+++ src/main/java/com/example/assignmnt1/CounterController.java
@@ -0,0 +1,11 @@
+package com.example.assignmnt1;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public interface CounterController {
+    void addModel(CounterModel var1);
+
+    void createGUI();
+}
